f24a2720cd630bcc439cc4368ef17e83853c1855
saeyama <camper_green_yellow_21@yahoo.co.jp>
2022-09-23 13:47:28 +0900

    0以下の数値が保存されないようにquantityにバリデーションを追加

--- app/models/part.rb
+++ app/models/part.rb
@@ -2,7 +2,7 @@
 
 class Part < ApplicationRecord
   validates :name, presence: true
-  validates :quantity, presence: true
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 
   belongs_to :design
 end
