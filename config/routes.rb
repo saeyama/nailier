4c392a28111475d8a651fc5cd4dc22019a14898b
saeyama <camper_green_yellow_21@yahoo.co.jp>
2022-12-16 09:10:18 +0900

    サインアップ後の遷移先ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   get 'privacy_policy', to: 'pages#privacy_policy', as: 'privacy_policy'
   get 'tos', to: 'pages#tos', as: 'tos'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get 'users/thanks', to: 'users/registrations#thanks'
+  end
+
   get 'users', to: 'users#show'
 
   resources :inquiries, only: [:new, :create]
