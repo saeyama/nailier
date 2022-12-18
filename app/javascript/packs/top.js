a35d1a1ca3ef853f1399d16cf53364b954b5915f
saeyama <camper_green_yellow_21@yahoo.co.jp>
2022-12-18 22:14:44 +0900

    不要なファイルを削除・topページのコンテンツを作成

--- app/javascript/packs/top.js
+++ app/javascript/packs/top.js
@@ -1,12 +1,25 @@
-import Vue from 'vue'
-import Top from '../top'
-
 document.addEventListener('DOMContentLoaded', () => {
-  const selector = '#js-top'
-  const top = document.querySelector(selector)
-  if (top) {
-    new Vue({
-      render: (h) => h(Top)
-    }).$mount(selector)
+  const fadeTargets = document.querySelectorAll('.scroll-fade-in')
+
+  const fadeOption = {
+    root: null,
+    rootMargin: '-10% 0px',
+    threshold: [0]
   }
+
+  const targets = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active')
+      } else {
+        entry.target.classList.remove('active')
+      }
+    })
+  }
+
+  const fadeObserver = new IntersectionObserver(targets, fadeOption)
+
+  fadeTargets.forEach((target) => {
+    fadeObserver.observe(target)
+  })
 })
