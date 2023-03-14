8a3589fdb8e740b10d94a52caa81fc66102e2894
saeyama <camper_green_yellow_21@yahoo.co.jp>
2023-03-14 21:39:29 +0900

    before_action :authenticate_userをapplication_controllerへ移動

--- app/controllers/api/designs_controller.rb
+++ app/controllers/api/designs_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Api::DesignsController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_design, only: %i[show edit update destroy]
   skip_before_action :verify_authenticity_token
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: %i[show]
-
   def show
     @hand = current_user.designs.where(nail_part: 'ハンド')
     @foot = current_user.designs.where(nail_part: 'フット')
